713d6538248055fbe49bde605430f0a548664877
Javed Chowdhury <j.chowdhury305@gmail.com>
2024-11-28 15:53:43 -0500

    home stlye

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import './styles/Home.css'
+
 export default function Home(){
     return (
-        <div>
+        <div className="container">
             <h1>Home</h1>
             <p>This is a powerlifitng application that allows you to calculate your one rep max and track your progress overtime</p>
             <p>You can also create an AI generated workout plan to hit your new personal records</p>
